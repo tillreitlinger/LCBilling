7bdd2485d38a46d52819d1020a5d21cd2678e833
PaulIVI <mailformachines01@gmail.com>
2020-11-14 17:12:16 +0100

    refactor functions for beter readability

--- src/main/scala/LC.scala
+++ src/main/scala/LC.scala
@@ -10,18 +10,10 @@ case class LC(roommates: String*) {
     generateBillDocument(bill, sum, locations)
   }
 
-  def generateSum(bill:Seq[Vector[Float]])={
-    val result = bill.toList.transpose.map(_.sum)
-    result
-  }
-
-
   def generateBillDocument(bill:Seq[Vector[Float]], sum: List[Float], location: Seq[String])={
-    val names = "\t" + roommates.map(person => person).mkString("\t") +"\n"
-    val billString = bill.map(outlay => outlay.map(amount => "\t" + f"$amount%1.2f")).zipWithIndex.map{
-      case (n, index) => n.mkString("") + "\t" + location(index)
-    }.mkString("\n")
-    val sumString = "\n\t" + sum.map(sumOfPerson => f"$sumOfPerson%1.2f").mkString("\t") + "\n"
+    val names = createNameString()
+    val billString = createBillString(bill, location)
+    val sumString = createSumString(sum)
     val resultString = getPayInstuctions(sum)
     names + billString + sumString +resultString
   }
@@ -29,7 +21,7 @@ case class LC(roommates: String*) {
   def getPayInstuctions( sum:List[Float])={
     val payInstructionString = roommates.zipWithIndex.map{
       case (mate, index) =>
-        if (sum(index) < 0){
+        if(personIsInDept(sum, index)){
           mate + " has to pay " + sum(index) + " Euro\n"
         }
         else{
@@ -38,8 +30,35 @@ case class LC(roommates: String*) {
     }
     payInstructionString.mkString("")
   }
-  def getInitalSituation(persons: Seq[String]): Vector[Float] ={
-    Vector.fill(persons.length)(0)
+
+  def getFinalSituation(initalSituation: Vector[Float], outlays: Seq[Outlay]):Seq[Vector[Float]] ={
+    val complete_list = outlays.map{
+      overlay => {
+        val payedFrom = roommates.indexOf(overlay.payedFrom.get)
+        val amount = overlay.amount.get
+        val numberOfIvolved = overlay.payedFor.get.length
+        val involvedPeople = overlay.payedFor.get.map(person => roommates.indexOf(person))
+        val subAmount = getSubstractionAmountForEachPerson(initalSituation, involvedPeople, numberOfIvolved, amount)
+        val final_row = addAmountToPersonWhoPayed(subAmount, payedFrom, amount)
+        final_row
+      }
+    }
+    complete_list
+  }
+
+  def addAmountToPersonWhoPayed(subAmount:Vector[Float], payedFrom:Int, amount:Float):Vector[Float]={
+    subAmount.zipWithIndex.map{
+      case (initialAmount, index) =>
+        if (payedFrom == index) initialAmount+amount
+        else initialAmount}
+  }
+
+  def getSubstractionAmountForEachPerson(initalSituation: Vector[Float], involvedPeople: Seq[Int], numberOfIvolved:Int, amount:Float):Vector[Float]={
+    initalSituation.zipWithIndex.map{
+      case (initalAmount, index) =>
+        if (involvedPeople.contains(index)) -amount/numberOfIvolved
+        else initalAmount
+    }
   }
 
   def getLocations(outlays: Seq[Outlay])={
@@ -48,25 +67,31 @@ case class LC(roommates: String*) {
     }
     locations
   }
-  def getFinalSituation(initalSituation: Vector[Float], outlays: Seq[Outlay]):Seq[Vector[Float]] ={
-    val complete_list = outlays.map{
-      overlay => {
-        val payedFrom = roommates.indexOf(overlay.payedFrom.get)
-        val amount = overlay.amount.get
-        val numberOfIvolved = overlay.payedFor.get.length
-        val involvedPeople = overlay.payedFor.get.map(person => roommates.indexOf(person))
-        val subAmount = initalSituation.zipWithIndex.map{
-          case (initalAmount, index) =>
-            if (involvedPeople.contains(index)) -amount/numberOfIvolved
-            else initalAmount
-        }
-        val final_row = subAmount.zipWithIndex.map{
-          case (initialAmount, index) =>
-            if (payedFrom == index) initialAmount+amount
-            else initialAmount}
-        final_row
-      }
-    }
-    complete_list
+
+  def generateSum(bill:Seq[Vector[Float]])={
+    val result = bill.toList.transpose.map(_.sum)
+    result
+  }
+
+  def createSumString(sum: List[Float])={
+    "\n\t" + sum.map(sumOfPerson => f"$sumOfPerson%1.2f").mkString("\t") + "\n"
+  }
+
+  def createNameString()={
+    "\t" + roommates.map(person => person).mkString("\t") +"\n"
+  }
+
+  def createBillString(bill:Seq[Vector[Float]], location: Seq[String])={
+    bill.map(outlay => outlay.map(amount => "\t" + f"$amount%1.2f")).zipWithIndex.map{
+      case (n, index) => n.mkString("") + "\t" + location(index)
+    }.mkString("\n")
+  }
+
+  def personIsInDept(sum:List[Float], index:Int)={
+    (sum(index) < 0)
+  }
+
+  def getInitalSituation(persons: Seq[String]): Vector[Float] ={
+    Vector.fill(persons.length)(0)
   }
 }
