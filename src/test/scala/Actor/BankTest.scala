3b84bd9fffd769a9f1c7f4c8f86ea50254264caf
tillreitlinger <Till-Reitlinger@outlook.de>
2020-11-30 10:19:43 +0100

    Added some tests for the BankActor.

--- src/test/scala/Actor/BankTest.scala
+++ src/test/scala/Actor/BankTest.scala
@@ -1,13 +1,14 @@
 import Messages.{CreateBankAccount, PrintBalance, Transaction}
 import akka.actor.{ActorSystem, Props}
 import akka.pattern.ask
-import akka.testkit.{ImplicitSender, TestKit}
+import akka.testkit.{ImplicitSender, TestActorRef, TestKit}
 import org.scalatest.BeforeAndAfterAll
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpecLike
 
 import scala.concurrent.duration.{Duration, DurationInt}
 import scala.concurrent.{Await, Future}
+
 
 class BankTest extends TestKit(ActorSystem("BankTest-System"))
 with ImplicitSender
@@ -43,5 +44,46 @@ with BeforeAndAfterAll {
       val billString : Future[Any]=bankActor.ask(PrintBalance)(10.seconds)
       Await.result(billString,Duration.Inf) should be(resultString)
     }
+    "add a lc member to the internal state when it gets a CreateBankAccount Message" in{
+      val silentActor = TestActorRef[Bank]
+      silentActor ! CreateBankAccount("Person1")
+      silentActor ! CreateBankAccount("Person2")
+      silentActor ! CreateBankAccount("Person3")
+      silentActor ! CreateBankAccount("Person4")
+      silentActor ! CreateBankAccount("Person5")
+
+      silentActor.underlyingActor.lcMembers.size should be(5)
+    }
   }
+  "In the bank actor, the" must{
+    "function calculateTransaction should return true when an outlay is passed" in{
+      val bankActor = TestActorRef[Bank]
+      bankActor ! CreateBankAccount("Person6")
+      bankActor ! CreateBankAccount("Person7")
+      val outlay = Outlay(payedFrom = Some("Person6"), payedFor = Some(Seq("Person6", "Person7")), amount = Some(100), at = Some("supermarket"))
+      bankActor.underlyingActor.calculateTransaction(outlay) should be(true)
+    }
+    "function calculateEveryWithdraw should return true when an outlay and costs are passed" in{
+      val bankActor = TestActorRef[Bank]
+      bankActor ! CreateBankAccount("Person6")
+      bankActor ! CreateBankAccount("Person7")
+      val outlay = Outlay(payedFrom = Some("Person6"), payedFor = Some(Seq("Person6", "Person7")), amount = Some(100), at = Some("supermarket"))
+      bankActor.underlyingActor.calculateEveryWithdraw(50, outlay) should be(true)
+    }
+    "function calculateEveryWithdraw should return false when an outlay with persons which are not in the lc are passed" in{
+      val bankActor = TestActorRef[Bank]
+      bankActor ! CreateBankAccount("Person6")
+      bankActor ! CreateBankAccount("Person7")
+      val outlay = Outlay(payedFrom = Some("Person5"), payedFor = Some(Seq("Person8", "Person7")), amount = Some(100), at = Some("supermarket"))
+      bankActor.underlyingActor.calculateEveryWithdraw(50, outlay) should be(false)
+    }
+    "function calculateDeposit should return true when an correct outlay is passed" in{
+      val bankActor = TestActorRef[Bank]
+      bankActor ! CreateBankAccount("Person6")
+      bankActor ! CreateBankAccount("Person7")
+      val outlay = Outlay(payedFrom = Some("Person6"), payedFor = Some(Seq("Person6", "Person7")), amount = Some(100), at = Some("supermarket"))
+      bankActor.underlyingActor.calculateDeposit(outlay) should be(true)
+    }
+  }
+
 }
