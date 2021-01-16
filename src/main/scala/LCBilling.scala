6d418f12868a3a64021e5e89dfb420406d76be06
tillreitlinger <Till-Reitlinger@outlook.de>
2021-01-16 15:49:27 +0100

    Minor changes.

--- src/main/scala/LCBilling.scala
+++ src/main/scala/LCBilling.scala
@@ -2,12 +2,16 @@ import scala.io.Source
 
 object LCBilling {
   def main(args: Array[String]) = {
+
     val input_from_txt = Source.fromFile("./src/text.txt").getLines.toList
+
     val billingParserModel = new BillingParserModel()
-    val lc = billingParserModel.generateLCFromTXTString(input_from_txt(0))
+    val lc = billingParserModel.generateLCFromTXTString(input_from_txt.head)
     val input_outlays_from_txt = input_from_txt.drop(1)
     val outlay_list = billingParserModel.generateOutlaysFromTXTStrings(input_outlays_from_txt)
+
     val bill = billingParserModel.generateBill(lc, outlay_list)
+
     print(bill)
   }
 }
